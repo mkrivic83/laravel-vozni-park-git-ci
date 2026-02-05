282439fa5411eb4b549cf692ae28f953c575572c
mkrivic83 <krivic.milan@gmail.com>
2026-02-05 18:10:12 +0100

    dodao vrijeme ispisa na stranicu vozila

--- resources/views/vozila/index.blade.php
+++ resources/views/vozila/index.blade.php
@@ -7,6 +7,7 @@
            class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
             + Novo vozilo
         </a>
+        <p>Vrijeme ispisa: {{ \Carbon\Carbon::now()->format('d.m.Y H:i:s') }}</p>
     </div>
 
     @if(session('status'))
